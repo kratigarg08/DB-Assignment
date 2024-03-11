c5a4f0aef324cae6ce0e0eb7e57f00ba041064d6
Krati Garg <106159758+kratigarg08@users.noreply.github.com>
2024-03-11 15:04:08 +0530

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -4,20 +4,6 @@ CREATE DATABASE shop;
 -- Switch to 'shop' database
 USE shop;
 
--- Create 'product' table
-CREATE TABLE product (
-    product_id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    description TEXT,
-    sku VARCHAR(50),
-    category_id INT,
-    inventory_id INT,
-    price DECIMAL(10, 2) NOT NULL,
-    discount_id INT,
-    FOREIGN KEY (category_id) REFERENCES product_category(category_id),
-    FOREIGN KEY (discount_id) REFERENCES discount(discount_id)
-);
-
 -- Create 'product_category' table
 CREATE TABLE product_category (
     category_id INT AUTO_INCREMENT PRIMARY KEY,
@@ -32,6 +18,20 @@ CREATE TABLE discount (
     description TEXT,
     discount_percentage DECIMAL(5, 2) NOT NULL,
     is_active BOOLEAN NOT NULL DEFAULT true
+);
+
+-- Create 'product' table
+CREATE TABLE product (
+    product_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    description TEXT,
+    sku VARCHAR(50),
+    category_id INT,
+    inventory_id INT,
+    price DECIMAL(10, 2) NOT NULL,
+    discount_id INT,
+    FOREIGN KEY (category_id) REFERENCES product_category(category_id),
+    FOREIGN KEY (discount_id) REFERENCES discount(discount_id)
 );
 
 -- Insert data into 'product_category' table
